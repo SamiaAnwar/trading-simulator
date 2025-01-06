623593bdb16526cc4c9240f7503b524ae4c5884c
SamiaAnwar <samia.anwar123@gmail.com>
2025-01-06 15:53:29 -0500

    Visualise feature importance and create model.pkl

--- backend/data-processing/dataProcessing.py
+++ backend/data-processing/dataProcessing.py
@@ -1,12 +1,16 @@
 from retrieval import retrieval
 from featureCreation import featureCreation
-from sklearn.model_selection import train_test_split
 from targetExtraction import targetExtraction
 from combineSymbols import combineSymbols
 from randomForestModel import randomForestModel
+from featureImportance import featureImportance
+from modelAssessment import assess
+from sklearn.model_selection import train_test_split
+import pickle
 import pandas as pd 
 #Data Retrieval 
 symbols = ["NVDA", "RGTI", "RIVN", "LCID", "TSLA", "AAPL", "F", "BA", "UBER", "FSLR", "GM", "MU", "PLTR", "WBA", "MRNA", "PFE", "EL", "ADBE", "GOOG", "AMZN", "MSFT"]
+one = ['NVDA']
 data = retrieval(symbols)
 #Feature Creation 
 features = featureCreation(data)
@@ -18,4 +22,14 @@ y = training['Close']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 #Getting Random Forest Model 
 rfmodel = randomForestModel(X_train, y_train)
+#Visualise Feature Importance
+featureImportance(feature_names, rfmodel)
+#Assess the Model 
+mae, mse, r2 = assess(rfmodel, X_test, y_test)
+print(f"Mean Absolute Error: {mae}")
+print(f"Mean Squared Error: {mse}")
+print(f"R-Squared: {r2}")
+with open('models/stock_predictor.pkl', 'wb') as f:
+    pickle.dump(rfmodel, f)
+
 
