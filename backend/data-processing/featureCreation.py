cd357b7e0fc5457da2be866c4026e2ab0312a74a
SamiaAnwar <samia.anwar123@gmail.com>
2025-01-06 13:56:21 -0500

    Removed NaN and invalid values when creating features

--- backend/data-processing/featureCreation.py
+++ backend/data-processing/featureCreation.py
@@ -1,5 +1,7 @@
 #The new features are derived from closing and volume values 
 #New Features includes: 
+from sklearn.preprocessing import StandardScaler
+
 def featureCreation(data, window=5):
     features = {}
     for symbol, df in data.items():
@@ -9,8 +11,16 @@ def featureCreation(data, window=5):
         df['Lag_1'] = df['Close'].shift(1)
         df['Lag_2'] = df['Close'].shift(2)
         df['Volume_Change'] = df['Volume'].pct_change()       
-        df['Close_Change'] = df['Close'].pct_change()         
-        features[symbol] = df[['MA_5', 'MA_10', 'Volume_Change', 'Close_Change', 'Close', 'EMA_5', 'Lag_1', 'Lag_2']]
+        df['Close_Change'] = df['Close'].pct_change()   
+        df.replace([float('inf'), float('-inf')], float('nan'), inplace=True)
+        df.dropna(inplace=True)
+
+        scaler = StandardScaler()
+        df[['MA_5', 'MA_10', 'Volume_Change', 'Close_Change', 'EMA_5', 'Lag_1', 'Lag_2']] = scaler.fit_transform(df[['MA_5', 'MA_10', 'Volume_Change', 'Close_Change', 'EMA_5', 'Lag_1', 'Lag_2']])
+      
+        features[symbol] = df
+        #df.to_csv(f"data/{symbol}_historical_data.csv")
+
     return features
 
 
