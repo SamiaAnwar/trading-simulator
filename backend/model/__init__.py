28c9a9dead656ec842b56a0724684a578903e3bf
SamiaAnwar <samia.anwar123@gmail.com>
2025-01-14 13:53:23 -0500

    model package initialiser

--- backend/model/__init__.py
+++ backend/model/__init__.py
@@ -1,4 +1,4 @@
-from train_model import train
+from model.train_model import train
 import pickle
 
 SYMBOLS = ["NVDA", "RGTI", "RIVN", "LCID", "TSLA", "AAPL", "F", "BA", "UBER", "FSLR", "GM", "MU", "PLTR", "WBA", "MRNA", "PFE", "EL", "ADBE", "GOOG", "AMZN", "MSFT"]
@@ -6,5 +6,5 @@ FEATURES = ['MA_5', 'MA_10', 'Volume_Change', 'Close_Change', 'EMA_5', 'Lag_1', 
 
 def create_model():
     model = train(SYMBOLS, FEATURES)
-    with open('predictor_model.pkl', 'wb') as f:
-        pickle.dump(model, f)
+    filename = 'model/predictor_model.pkl'
+    pickle.dump(model, open(filename, 'wb'))
