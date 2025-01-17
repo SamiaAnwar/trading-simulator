ae6a4bfc374f58156a10c690fb18cc437001517b
SamiaAnwar <samia.anwar123@gmail.com>
2025-01-17 17:19:52 -0500

    Added features

--- backend/model/__init__.py
+++ backend/model/__init__.py
@@ -2,7 +2,7 @@ from model.train_model import train
 import pickle
 
 SYMBOLS = ["NVDA", "RGTI", "RIVN", "LCID", "TSLA", "AAPL", "F", "BA", "UBER", "FSLR", "GM", "MU", "PLTR", "WBA", "MRNA", "PFE", "EL", "ADBE", "GOOG", "AMZN", "MSFT"]
-FEATURES = ['MA_5', 'MA_10', 'Volume_Change', 'Close_Change', 'EMA_5', 'Lag_1', 'Lag_2']
+FEATURES = ['MA_5', 'MA_10', 'Volume_Change', 'Close_Change', 'EMA_5', 'Lag_1', 'Lag_2', 'Close', 'Volume']
 
 def create_model():
     model = train(SYMBOLS, FEATURES)
