2cb3d53248bd10fa6553f492ba40f4487d13e2fe
SamiaAnwar <samia.anwar123@gmail.com>
2025-01-20 15:41:59 -0500

    fetching information from yfinance and added relevant features

--- backend/app/data_fetcher.py
+++ backend/app/data_fetcher.py
@@ -1,25 +1,40 @@
-import requests
-import os 
-API_KEY = os.getenv('ALPHA_VANTAGE_API_KEY')
+import yfinance as yf 
+import pandas as pd
+from datetime import date, timedelta
+
+
+FEATURES = ['MA_5', 'MA_10', 'Volume_Change', 'Close_Change', 'EMA_5', 'Lag_1', 'Lag_2', 'Close', 'Volume']
 def get_live_data(symbols):
-    url = 'https://www.alphavantage.co/query'
-    
     data = {}
     for symbol in symbols:
-        params = {
-            'function': 'TIME_SERIES_INTRADAY',
-            'symbol': symbol,
-            'interval': '1min',
-            'apikey': API_KEY
-        }
-        response = requests.get(url, params=params)
-        symbol_data = response.json()
 
-        if 'Time Series (1min)' in symbol_data:
-            latest_time = list(symbol_data['Time Series (1min)'].keys())[0]
-            latest_data = symbol_data['Time Series (1min)'][latest_time]
-            data[symbol] = {
-                'close': float(latest_data['4. close']),
-                'volume':int(latest_data['5. volume'])
-            }
+        #Fetching Live Data 
+        df = yf.Ticker(symbol).history(start=date.today() - timedelta(22), end=date.today() - timedelta(1), interval='1d')
+        df.reset_index(inplace=True)
+        
+        #Extracting relevant Feature Data 
+        for feature in FEATURES:
+            match feature:
+                case 'MA_5':
+                    df['MA_5'] = df['Close'].rolling(window=5).mean()
+                case 'MA_10':
+                    df['MA_10'] = df['Close'].rolling(window=10).mean()
+                case 'EMA_5':
+                    df['EMA_5'] = df['Close'].ewm(span=5, adjust=False).mean()
+                case 'Lag_1':
+                    df['Lag_1'] = df['Close'].shift(1)
+                case 'Lag_2':
+                    df['Lag_2'] = df['Close'].shift(2)
+                case 'Volume_Change':
+                    df['Volume_Change'] = df['Volume'].pct_change()
+                case 'Close_Change':       
+                    df['Close_Change'] = df['Close'].pct_change() 
+                case 'Datetime':
+                    df['Datetime'] = df['Datetime'].astype(str)
+        df.replace([float('inf'), float('-inf')], float('nan'), inplace=True)
+        df.dropna(inplace=True)
+
+        #Getting the latest 
+        data[symbol] = df[FEATURES].iloc[-1].to_json()
+
     return data
