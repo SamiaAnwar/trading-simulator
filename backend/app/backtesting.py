e728ea08ae9a1deaf56d38c4a4fda55ea6bfcae4
SamiaAnwar <samia.anwar123@gmail.com>
2025-01-22 13:11:22 -0500

    mapped out backtesting framework

---
+++ backend/app/backtesting.py
@@ -0,0 +1,13 @@
+from app.data_fetcher import get_live_data, get_features
+from datetime import date, timedelta
+
+#Input: symbols
+#Output: trade_history, profit_history 
+
+def backtest(symbols):
+    #get closing prices from [31, 60] days ago
+    #get their features 
+    #make predictions 
+    #get closing prices from [0, 30] days ago 
+    #execute a trade using price from 
+    return 
