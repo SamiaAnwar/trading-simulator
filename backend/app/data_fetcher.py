fb52f0d836424acabca5a0a99117e33fc3473870
SamiaAnwar <samia.anwar123@gmail.com>
2025-01-22 14:57:06 -0500

    live data and feature fetching for single symbol

--- backend/app/data_fetcher.py
+++ backend/app/data_fetcher.py
@@ -7,42 +7,36 @@ from datetime import date, timedelta
 FEATURES = ['Close', 'EMA_5', 'MA_5', 'Lag_2', 'MA_10', 'Lag_1']
 
 def get_features(data, features):
-    feature_data = {}
-    for symbol in data: 
-        df = data[symbol]
-        for feature in features:
-            match feature:
-                case 'MA_5':
-                    df['MA_5'] = df['Close'].rolling(window=5).mean()
-                case 'MA_10':
-                    df['MA_10'] = df['Close'].rolling(window=10).mean()
-                case 'EMA_5':
-                    df['EMA_5'] = df['Close'].ewm(span=5, adjust=False).mean()
-                case 'Lag_1':
-                    df['Lag_1'] = df['Close'].shift(1)
-                case 'Lag_2':
-                    df['Lag_2'] = df['Close'].shift(2)
-                case 'Volume_Change':
-                    df['Volume_Change'] = df['Volume'].pct_change()
-                case 'Close_Change':       
-                    df['Close_Change'] = df['Close'].pct_change() 
-                case 'Datetime':
-                    df['Datetime'] = df['Datetime'].astype(str)
-        df.replace([float('inf'), float('-inf')], float('nan'), inplace=True)
-        df.dropna(inplace=True)
-        feature_data[symbol] = df[FEATURES].iloc[-1].to_json()
+    df = {}
+    df['Close'] = data
+    df = pd.DataFrame(df)
+    for feature in features:
+        match feature:
+            case 'MA_5':
+                df['MA_5'] = df['Close'].rolling(window=5).mean()
+            case 'MA_10':
+                df['MA_10'] = df['Close'].rolling(window=10).mean()
+            case 'EMA_5':
+                df['EMA_5'] = df['Close'].ewm(span=5, adjust=False).mean()
+            case 'Lag_1':
+                df['Lag_1'] = df['Close'].shift(1)
+            case 'Lag_2':
+                df['Lag_2'] = df['Close'].shift(2)
+            case 'Volume_Change':
+                df['Volume_Change'] = df['Volume'].pct_change()
+            case 'Close_Change':       
+                df['Close_Change'] = df['Close'].pct_change() 
+            case 'Datetime':
+                df['Datetime'] = df['Datetime'].astype(str)
+    df.replace([float('inf'), float('-inf')], float('nan'), inplace=True)
+    df.dropna(inplace=True)
+    df.reset_index(inplace=True)
+    return pd.DataFrame(df[features].iloc[-1])
 
-def get_live_data(symbols, start, end):
-    data = {}
-    for symbol in symbols:
-        #Fetching Live Data 
-        #df = yf.Ticker(symbol).history(start=date.today() - timedelta(85), end=date.today() - timedelta(64), interval='1d')
-        #df = yf.Ticker(symbol).history(start=date.today() - timedelta(22), end=date.today() - timedelta(1), interval='1d')
-        df = yf.Ticker(symbol).history(start=start, end=end, interval='1d')
-        df.reset_index(inplace=True)
-        #Getting the latest 
-        data[symbol] = df
-    return data
+def get_live_data(symbol, start, end):
+    df = yf.Ticker(symbol).history(start=start, end=end, interval='1d')
+    df.reset_index(inplace=True)
+    return df['Close']
 
 def get_live_data_features(symbols):
     data = {}
