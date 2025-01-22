e2b7802aac5165f8c465f7a99aca0559ed1bb9da
SamiaAnwar <samia.anwar123@gmail.com>
2025-01-22 13:09:00 -0500

    added a predict route

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 import json
 from app.data_fetcher import get_live_data
+from app.predictor import predict, trade_decision
 
 app_routes = Blueprint('app_routes', __name__)
 
@@ -10,3 +11,13 @@ def live_data():
     data = get_live_data(symbols)
     data_json = json.dumps(data, indent=4)
     return data_json
+
+@app_routes.route('/predict', methods=['GET', 'POST'])
+def make_prediction():
+    data = request.json()
+    print(data)
+    model_input = json.loads(data)
+    prediction = predict(model_input)
+    decision = trade_decision(prediction)
+    
+    return jsonify(decision)
