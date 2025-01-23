4d059aaee82862630cf3a41035315f4ef5286ecc
SamiaAnwar <samia.anwar123@gmail.com>
2025-01-23 14:00:28 -0500

    added backtesting routes

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,8 +2,9 @@ from flask import Blueprint, request, jsonify
 import json
 from app.data_fetcher import get_live_data
 from app.predictor import predict, trade_decision
-
+from app.backtesting import backtest
 app_routes = Blueprint('app_routes', __name__)
+
 
 @app_routes.route("/get_live_data", methods=['GET', 'POST'])
 def live_data():
@@ -19,5 +20,16 @@ def make_prediction():
     model_input = json.loads(data)
     prediction = predict(model_input)
     decision = trade_decision(prediction)
-    
     return jsonify(decision)
+
+@app_routes.route('/backtest/portfolio_data', methods=['GET', 'POST'])
+def get_portfolio_data():
+    symbols = request.args.getlist('symbol', type=str)
+    portfolio_values, _ = backtest(symbols)
+    return jsonify(portfolio_values)
+
+@app_routes.route('/backtest/trade_history', methods=['GET', 'POST'])
+def get_trade_data():
+    symbols = request.args.getlist('symbol', type=str)
+    _, trade_history = backtest(symbols)
+    return jsonify(trade_history)
