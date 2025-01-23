852937b949bde4d9765741fe836323a49bc22a4b
SamiaAnwar <samia.anwar123@gmail.com>
2025-01-23 13:49:57 -0500

    trade execution and appropriate outputs

--- backend/app/backtesting.py
+++ backend/app/backtesting.py
@@ -1,5 +1,6 @@
 from data_fetcher import get_live_data, get_features
 from predictor import predict, trade_decision
+from trade import portfolio, execute_trade, calculate_portfolio_value
 from datetime import date, timedelta
 import pandas as pd
 
@@ -8,11 +9,12 @@ import pandas as pd
 FEATURES = ['Close', 'EMA_5', 'MA_5', 'Lag_2', 'MA_10', 'Lag_1']
 
 def backtest(symbols):
+    
     # 1) Get features from [31, 60] trading days ago
     past_features = {} #Contains daily datapoints from the past ~30 trading days 
     for symbol in symbols:
         sym_feats = []
-        df = get_live_data(symbol, date.today() - timedelta(113+42), date.today() - timedelta(57))
+        df, _ = get_live_data(symbol, date.today() - timedelta(113+42), date.today() - timedelta(57))
         df = df.tail(40) 
         for i in range(30):
             feature_data = get_features(df[i:i+10], FEATURES)
@@ -35,12 +37,24 @@ def backtest(symbols):
     # 3) Get closing prices from [0, 30] days ago 
     curr_mo = {}
     for symbol in symbols:
-        curr_mo[symbol] = get_live_data(symbol, date.today() - timedelta(56), date.today()).head(30)
-    print(len(decisions['AAPL']))
-    print(len(predictions['AAPL']))  
-    print(len(curr_mo['AAPL']))  
+        curr_mo[symbol], dates = get_live_data(symbol, date.today() - timedelta(56), date.today())
+        curr_mo[symbol] = curr_mo[symbol].head(30)
+        dates = dates.head(30)
 
-    #Execute a trade using price from 3) and decision from 2) 
-    return 
+    # 4) Execute a trade using price from 3) and decision from 2) 
+    portfolio_values = [0]*30
+    trade_history = []
+    for i in range(len(decisions[symbol])):
+        for symbol in symbols: 
+            trade = {}
+            execute_trade(portfolio, symbol, decisions[symbol][i], 1, curr_mo[symbol][i])
+            trade['symbol'] = symbol
+            trade['action'] = decisions[symbol][i]
+            trade['date'] = dates[i]
+            trade['price'] = curr_mo[symbol][i]
+            trade_history.append(trade)
+        portfolio_values[i] = (calculate_portfolio_value(portfolio, curr_mo, i))
 
-backtest(['AAPL', 'UBER'])
+    return portfolio_values, trade_history
+
+backtest(["RIVN", "TSLA", "AAPL"])
