55f6899a00725cb02beb7823a982878da89f5921
SamiaAnwar <samia.anwar123@gmail.com>
2025-01-25 13:15:00 -0500

    added dependancy array for one time fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,7 @@ function App() {
       console.error("Error fetching portfolio data:", error);
     });
 
-  })
+  }, [])
   return (
     <div className="min-h-screen bg-gray-100 p-6">
       <h1 className="text-3xl font-bold text-center text-blue-500 mb-8">
