6b406fefb151f614e7576a01824980086c4e950b
SamiaAnwar <samia.anwar123@gmail.com>
2025-01-26 13:30:28 -0500

    created a page to display data

---
+++ frontend/src/pages/TradingResults.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react"; 
+import { useLocation } from "react-router"; 
+import LineGraph from "../components/LineGraph";
+import DataTable from "../components/DataTable";
+import axios from "axios";
+
+const TradingResults = () => {
+    const location = useLocation(); 
+    const { selectedOptions } = location.state || { selectedOptions: [] };
+    const query = selectedOptions.join("&symbol=")
+    const [portfolioData, setPortfolioData] = useState([]);
+    const [tradeData, setTradeData] = useState([]);
+
+    useEffect(() => {
+        axios.get(`/backtest?symbol=${query}`).then(response => (
+            setTradeData(response.data)
+        )).catch(error => {
+            console.error("Error fetching trading data:", error);
+        }).then(response => (
+            setPortfolioData(response.data)
+        )).catch(error => {
+            console.error("Error fetching portfolio data:", error);
+        });
+    }, []); 
+    return (
+        <div className="min-h-screen bg-gray-100 p-6">
+          <h1 className="text-3xl font-bold text-center text-blue-500 mb-8">
+            Stock Market Predictor
+          </h1>
+          <div className='bg-white p-6 shadow-lg rounded-lg object-cover'>
+            <LineGraph data={portfolioData} />
+          </div>
+          <div className='mt-6 bg-white p-6 shadow-lg rounded-lg'>
+            <DataTable data={tradeData} />
+          </div>
+        </div>
+    );
+}; 
+
+export default TradingResults; 
