43e2b37dacdeadeff547042d26564c57b2674b2b
SamiaAnwar <samia.anwar123@gmail.com>
2025-01-26 13:31:57 -0500

    added routing to the new pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,18 @@
-import React, { useState, useEffect } from 'react'
-import LineGraph from './components/LineGraph';
-import DataTable from './components/DataTable';
-import axios from "axios";
+import React from 'react'; 
+import { BrowserRouter as Router, Routes, Route } from "react-router"; 
+import SymbolSelection from './pages/SymbolSelection';
+import TradingResults from './pages/TradingResults';
 
 import './App.css';
 
-function App() {
-  const [portfolioData, setPortfolioData] = useState([]);
-  const [tradeData, setTradeData] = useState([]);
-
-  useEffect(() => {
-    axios.get("/backtest/trade_history?symbol=AAPL&symbol=GOOG&symbol=UBER").then(response => (
-      setTradeData(response.data)
-    )).catch(error => {
-      console.error("Error fetching portfolio data:", error);
-    });
-    axios.get("/backtest/portfolio_data?symbol=AAPL&symbol=GOOG&symbol=UBER").then(response => (
-      setPortfolioData(response.data)
-    )).catch(error => {
-      console.error("Error fetching portfolio data:", error);
-    });
-
-  }, [])
+const App = () => {
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold text-center text-blue-500 mb-8">
-        Stock Market Predictor
-      </h1>
-      <div className='bg-white p-6 shadow-lg rounded-lg'>
-        <LineGraph data={portfolioData} />
-      </div>
-      <div className='mt-6 bg-white p-6 shadow-lg rounded-lg'>
-        <DataTable data={tradeData} />
-      </div>
-    </div>
-  );
-}
-
+    <Router>
+      <Routes>
+        <Route path="/" element={<SymbolSelection />} />
+        <Route path="/results" element={<TradingResults/>} />
+      </Routes>
+    </Router>
+  ); 
+}; 
 export default App;
