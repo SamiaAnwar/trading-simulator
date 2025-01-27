07169dc0880d219a0f8fb55de53a57eb76a45975
SamiaAnwar <samia.anwar123@gmail.com>
2025-01-27 15:12:08 -0500

    dropdown to pass new selection

---
+++ frontend/src/components/DropDown.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react"; 
+
+const DropDown = ({ options, onSelect }) => {
+    const [selectedItem, setSelectedItem] = useState(options[0] || "");
+    function handleChange( event ) {
+        setSelectedItem(event.target.value);
+        onSelect(selectedItem)
+    };
+    return (
+            
+            <select
+                id="dropdown"
+                value={selectedItem}
+                onChange={handleChange}
+                className="block w-1/4 bg-white border border-gray-300 rounded-md shadow-sm px-4 py-2 focus:outline-none focus:ring focus:ring-indigo-500 focus:border-indigo-500"
+            >
+                {options.map((option, index) => (
+                    <option key={index} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+    
+    );
+
+};
+
+export default DropDown; 
