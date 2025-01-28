57cf6e3388924815fe93fcf81abafcf9a7f077e0
SamiaAnwar <samia.anwar123@gmail.com>
2025-01-28 14:22:44 -0500

    delete ML folder

--- backend/ML/combineSymbols.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-def combineSymbols(features):
-    combined = None
-    for s, df in features.items():
-        if combined is None :
-            combined = df
-        else:
-            combined = pd.merge(combined, df, on='Date', how='outer', suffixes=('', f'_{s}'))
-    return combined
