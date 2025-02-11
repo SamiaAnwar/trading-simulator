3d8db597ecfa4819b799e5982ac10496ef649571
SamiaAnwar <samia.anwar123@gmail.com>
2025-02-11 13:42:17 -0500

    endpoint api framework for live page data

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,6 +2,27 @@ from flask import Blueprint, request, jsonify
 from app.backtesting import trades, portfolio_value, compare_predictions
 app_routes = Blueprint('app_routes', __name__)
 
+@app_routes.route('/live/scheduled_update', methods=['PUT'])
+def scheduled_update():
+    #TODO: Make a decision 
+    #TODO: Update the databases 
+    return 
+
+@app_routes.route('/live/portfolio', methods=['GET', 'POST'])
+def get_live_portfolio():
+    #TODO: Fetch from portfolio table   
+    return 
+
+@app_routes.route('/live/value_history', methods=['GET', 'POST'])
+def get_value_history():
+    #TODO: Fetch from value history table  
+    return 
+
+@app_routes.route('/live/trade_history', methods=['GET', 'POST'])
+def get_trade_history():
+    #TODO: Fetch from trade history table  
+    return 
+
 
 @app_routes.route('/backtest/portfolio_data', methods=['GET', 'POST'])
 def get_portfolio_data():
