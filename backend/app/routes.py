83407f066bb411e7abae27e0153d17c7e2eaf91b
SamiaAnwar <samia.anwar123@gmail.com>
2025-02-18 13:24:37 -0500

    added fetch calls to relevant apis

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,28 +1,45 @@
 from flask import Blueprint, request, jsonify
 from app.backtesting import trades, portfolio_value, compare_predictions
+import os
+from supabase import create_client, Client
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+user = os.environ.get("USER_ID")
+user = 1
+
 app_routes = Blueprint('app_routes', __name__)
 
-@app_routes.route('/live/scheduled_update', methods=['PUT'])
+@app_routes.route('/live/scheduled_update', methods=['GET', 'PUT'])
 def scheduled_update():
-    #TODO: Make a decision 
-    #TODO: Update the databases 
-    return 
+    portfolio, _ = supabase.table("Portfolio").select("*").eq("user_id", user).execute()
+    cash = portfolio[1][0]['cash']
+    holdings = portfolio[1][0]['holdings']
+    #TODO: Make a decision
+
+    #TODO: Update the databases (incl. PortfolioValues, Portfolio > holdings, Trades)
+    
+    return {'Hello':cash}
 
 @app_routes.route('/live/portfolio', methods=['GET', 'POST'])
 def get_live_portfolio():
     #TODO: Fetch from portfolio table   
-    return 
+    portfolio, _ = supabase.table("Portfolio").select("*").eq("user_id", user).execute()
+    holdings = portfolio[1][0]['holdings']
+    return holdings
 
 @app_routes.route('/live/value_history', methods=['GET', 'POST'])
 def get_value_history():
     #TODO: Fetch from value history table  
-    return 
+    values, _ = supabase.table("PortfolioValues").select("value, date").eq("user_id", user).execute()
+    return jsonify(values[1])
 
 @app_routes.route('/live/trade_history', methods=['GET', 'POST'])
 def get_trade_history():
     #TODO: Fetch from trade history table  
-    return 
-
+    trades, _ = supabase.table("Trades").select("ticker, date, value").eq("user_id", user).execute()
+    return jsonify(trades[1])
 
 @app_routes.route('/backtest/portfolio_data', methods=['GET', 'POST'])
 def get_portfolio_data():
