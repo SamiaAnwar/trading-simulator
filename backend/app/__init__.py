729c188e2498946af60b470ff058f2328deacf99
SamiaAnwar <samia.anwar123@gmail.com>
2025-03-26 11:20:55 -0400

    fixed scheduling

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
-from app.routes import app_routes
+from app.routes import app_routes, scheduled_update
+from apscheduler.schedulers.background import BackgroundScheduler
 
 def create_app():
     app = Flask(__name__) 
     app.register_blueprint(app_routes)
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(scheduled_update, 'interval', days=1)
+    scheduler.start()
     return app 
